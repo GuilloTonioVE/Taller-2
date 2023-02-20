7f87fd958840ea9edadc7d30ed0570a9ad580420
GuilloTonioVE <111136599+GuilloTonioVE@users.noreply.github.com>
2023-02-20 00:00:03 -0500

    Update

--- src/modelo/Ingrediente.java
+++ src/modelo/Ingrediente.java
@@ -4,18 +4,21 @@ import java.io.File;
 
 public class Ingrediente {
 	private String nombre;
-	private Integer costoAdicional;
+	private Double costoAdicional;
+	private Map<String, Ingrediente> ingredientes;
 	public final static String archivoIngredientes = "."+File.separator+"data"+File.separator+"ingredientes.txt";
 	
-	public Ingrediente(String nombre, int costoAdicional){
-		
+	public Ingrediente(String nombre, Double costoAdicional){
+		this.nombre = nombre;
+		this.costoAdicional = costoAdicional;
+		//ingredientes = new Map<nombre, costoAdicional>();
 	}
 	
+
 	public String getNombre() {
 		return nombre;
 	}
-	public Integer getcostoAdicional() {
+	public Double getcostoAdicional() {
 		return costoAdicional;
 	}
-
 }
